8cf5e7a15aa27875073e000736d5928e8364da2c
Scott Lawan <lawanscott@gmail.com>
2025-03-30 20:18:58 -0500

    test toggling

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,11 @@
 
 // src="/test.html" for local
 // src="/mac_ciao/test.html" for deploy
-function myfunction(e) { 
-	e.preventDefault();
+
+// comment out for testing. get around no "e" defined reference . . .
+// function myfunction(e) { 
+// 	e.preventDefault();
+function myfunction() { 
 	var iViewer = `<iframe
 		src="/test.html"
 		name="targetframe"
@@ -16,3 +19,43 @@ function myfunction(e) {
 
 	document.getElementById('uv').innerHTML = iViewer;
 } 
+
+// testing toggle
+function myfunction_b() { 
+	var iViewer = `<iframe
+		src="/test_b.html"
+		name="targetframe"
+		allowTransparency="true"
+		scrolling="yes"
+		frameborder="0"
+		style="height: 100%; width: 100%"
+	>
+	</iframe>`
+
+	document.getElementById('uv').innerHTML = iViewer;
+} 
+
+const button = document.getElementById('myButton');
+let toggle = false;
+
+function functionA() {
+  console.log('Function A executed');
+}
+
+function functionB() {
+  console.log('Function B executed');
+}
+
+button.addEventListener('click', function() {
+  if (toggle) {
+    functionB();
+    toggle = false;
+    myfunction_b()
+  } else {
+    functionA();
+    toggle = true;
+    myfunction()
+  }
+});
+
+// Click Function to change the display value of the input
